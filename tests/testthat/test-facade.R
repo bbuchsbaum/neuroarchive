b92e4556bc1c3b5181698661a439c8bd21494bd5
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-05-24 00:54:40 -0400

    Default LNAFacade to last output

--- tests/testthat/test-facade.R
+++ tests/testthat/test-facade.R
@@ -12,3 +12,12 @@ test_that("LNAFacade writes and reads", {
   res <- fac$read(tmp)
   expect_true(inherits(res, "DataHandle"))
 })
+
+test_that("LNAFacade$read uses last_output when file missing", {
+  fac <- LNAFacade$new()
+  tmp <- local_tempfile(fileext = ".h5")
+  invisible(fac$write(array(2, dim = c(1,1,1)), tmp, transforms = character()))
+  expect_identical(fac$last_output, tmp)
+  res <- fac$read()
+  expect_true(inherits(res, "DataHandle"))
+})
