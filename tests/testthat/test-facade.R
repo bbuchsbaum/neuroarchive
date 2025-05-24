7326a88aa6a4d5eaa46f0f7d5ad24a7795669327
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-05-24 00:55:33 -0400

    Add write_pipeline method to LNAFacade

--- tests/testthat/test-facade.R
+++ tests/testthat/test-facade.R
@@ -12,3 +12,28 @@ test_that("LNAFacade writes and reads", {
   res <- fac$read(tmp)
   expect_true(inherits(res, "DataHandle"))
 })
+
+test_that("LNAFacade$write_pipeline forwards to lna_write", {
+  fac <- LNAFacade$new()
+  arr <- array(1, dim = c(1,1,1))
+  pipe <- as_pipeline(arr)
+
+  captured <- list()
+  fake_result <- list(file = "out.h5")
+
+  local_mocked_bindings(
+    lna_write = function(pipe_arg, file, ...) {
+      captured$pipe <<- pipe_arg
+      captured$file <<- file
+      fake_result
+    },
+    .env = asNamespace("neuroarchive")
+  )
+
+  res <- fac$write_pipeline(pipe, file = "out.h5")
+
+  expect_identical(captured$pipe, pipe)
+  expect_equal(captured$file, "out.h5")
+  expect_equal(fac$last_output, "out.h5")
+  expect_identical(res, "out.h5")
+})
